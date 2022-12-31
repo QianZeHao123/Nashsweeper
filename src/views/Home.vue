acf2e11e021687bb90fdd4b96cb48c50db1fed88
Qian Zehao <qianzehao123@outlook.com>
2022-12-31 12:11:18 +0800

    add DataBarShow and SideBar component in the homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,55 +4,21 @@
             <div class="grid grid-cols-4 gap-1 w-full">
                 <!-- 左侧Tabbar -->
                 <SideBar />
-
-
-
                 <!-- 右侧数据展示页面 -->
-                <div class="col-span-3 h-full border-4">
-
-
-                    <!-- <div class="grid grid-rows-3 gap-1">
-                        <div class="row-span-2">1</div>
-                        <div class="row-span-1">1</div>
-                    </div> -->
-                    <!-- <div class="h-2/3 border-4">
-                        <div class="grid grid-cols-3 gap-0">
-                            <div class="border col-span-2">1</div>
-                            <div class="h-96 border">1</div>
-                        </div>
-                    </div>
-                    <div class="h-1/3 border-4">
-                        <div class="grid grid-cols-3 gap-0">
-                            <div class="border h-72">1</div>
-                            <div class="border h-72">1</div>
-                            <div class="border h-72">1</div>
-                        </div>
-                    </div> -->
-                    <div class="grid grid-rows-3 grid-cols-3 gap-2">
-                        <div class="row-span-2 col-span-2 border-4">PartD</div>
-                        <div class="row-span-2 border-4">PartE</div>
-                        <div class="h-72 border-4">C-1</div>
-                        <div class="h-72 border-4">C-2</div>
-                        <div class="h-72 border-4">C-3</div>
-                    </div>
-
-
-                </div>
-
-
-
+                <DataBarShow />
             </div>
-
         </div>
     </div>
 </template>
 
 <script>
 import SideBar from "../components/SideBar.vue"
+import DataBarShow from "../components/DataBarShow.vue"
 export default {
     name: "Home",
     components: {
         SideBar,
+        DataBarShow
     },
     data() {
         return {}
