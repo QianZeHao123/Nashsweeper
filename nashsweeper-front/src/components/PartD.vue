0e2678b69b7eb7f8b5708cd73aee428c395d3051
Qian Zehao <qianzehao123@outlook.com>
2023-01-01 23:59:07 +0800

    PartD Com

---
+++ nashsweeper-front/src/components/PartD.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="border">Part-D</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PartD",
+    components: {},
+    data() {
+        return {}
+    },
+    methods: {},
+    mounted() { },
+}
+</script>
+
+<style scoped>
+
+</style>
