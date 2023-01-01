74c21e1cb9127c33d0dfb14cd369e6334af51e78
Qian Zehao <qianzehao123@outlook.com>
2023-01-01 23:59:40 +0800

    Vue main page

---
+++ nashsweeper-front/src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="mockup-window border bg-base-90 w-full">
+            <div class="grid grid-cols-4 gap-1 w-full">
+                <!-- 左侧Tabbar -->
+                <SideBar />
+                <!-- 右侧数据展示页面 -->
+                <DataBarShow />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import SideBar from "../components/SideBar.vue"
+import DataBarShow from "../components/DataBarShow.vue"
+export default {
+    name: "Home",
+    components: {
+        SideBar,
+        DataBarShow
+    },
+    data() {
+        return {}
+    },
+    methods: {},
+    mounted() { },
+}
+</script>
+
+<style scoped>
+
+</style>
