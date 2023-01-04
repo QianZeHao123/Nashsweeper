88d738514e33e54bb3b7fb5ca8827e3e9d403c2d
Qian Zehao <qianzehao123@outlook.com>
2023-01-04 10:37:28 +0800

    add CORS(backend) to vite config file

--- nashsweeper-front/vite.config.js
+++ nashsweeper-front/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    // port: 1234,
+    proxy: {
+      "/backend": {
+        target: "http://127.0.0.1:6778",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/backend": "/backend",
+        },
+      },
+    },
+  },
+});
