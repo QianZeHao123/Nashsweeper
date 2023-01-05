e587acf16fae21042f39a51f3fa8bd9b98e4be6e
Qian Zehao <qianzehao123@outlook.com>
2023-01-05 20:03:06 +0800

    PartC_Com dev

--- nashsweeper-front/src/components/PartC.vue
+++ nashsweeper-front/src/components/PartC.vue
@@ -1,6 +1,43 @@
 <template>
-    <div>
-        <h1 class="border">Part-C</h1>
+    <div class="flex justify-center items-center">
+        <div class="w-11/12 overflow-auto">
+
+            <div tabindex="0" class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+                <div class="collapse-title text-xl font-medium">
+                    Nash Equilibria (NE)
+                </div>
+                <div class="collapse-content">
+                    <div>Nash Equilibrium 1</div>
+                    <div>Nash Equilibrium 2</div>
+                    <div>Nash Equilibrium 3</div>
+                    <div>......</div>
+                </div>
+            </div>
+
+            <div tabindex="0" class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+                <div class="collapse-title text-xl font-medium">
+                    Best Responses (BR)
+                </div>
+                <div class="collapse-content">
+                    <div>Best Response 1</div>
+                    <div>Best Response 2</div>
+                    <div>Best Response 3</div>
+                    <div>Best Response 4</div>
+                </div>
+            </div>
+
+            <div tabindex="0" class="collapse collapse-plus border border-base-300 bg-base-100 rounded-box">
+                <div class="collapse-title text-xl font-medium">
+                    Steps
+                </div>
+                <div class="collapse-content">
+                    <div>Step 1</div>
+                    <div>Step 2</div>
+                    <div>......</div>
+                </div>
+            </div>
+
+        </div>
     </div>
 </template>
 
