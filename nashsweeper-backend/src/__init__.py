fbbcaecfedfba7579a1fe9485c559e3258580051
Qian Zehao <qianzehao123@outlook.com>
2023-01-05 08:49:35 +0800

    factory function

--- nashsweeper-backend/src/__init__.py
+++ nashsweeper-backend/src/__init__.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask
 
 
@@ -8,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        DATABASE=os.path.join(app.instance_path, 'nashsweeper.sqlite'),
     )
 
     if test_config is None:
@@ -24,9 +23,11 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
+
+    from . import db
+    db.init_app(app)
 
     return app
