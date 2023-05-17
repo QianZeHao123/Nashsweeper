525c8fe2b05f1534dfffda477d1ebad52a3402bc
Qian Zehao <qianzehao123@outlook.com>
2023-05-17 13:39:30 +0800

    add iPad version to main router

--- nashsweeper-front/src/router/index.js
+++ nashsweeper-front/src/router/index.js
@@ -1,12 +1,22 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-
+import Pad from "../views/Pad.vue";
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/",
       name: "Home",
+      component: Pad,
+    },
+    {
+      path: "/Pad",
+      name: "Pad",
+      component: Pad,
+    },
+    {
+      path: "/PC",
+      name: "PC version",
       component: Home,
     },
   ],
