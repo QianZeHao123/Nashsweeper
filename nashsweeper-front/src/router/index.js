8e792a7948c7d7db641d58c2242fa82bd492b0ae
Qian Zehao <qianzehao123@outlook.com>
2023-05-19 16:48:59 +0800

    delete iPad end view from PC

--- nashsweeper-front/src/router/index.js
+++ nashsweeper-front/src/router/index.js
@@ -1,18 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import Pad from "../views/Pad.vue";
+
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
       path: "/",
       name: "Home",
-      component: Pad,
-    },
-    {
-      path: "/Pad",
-      name: "Pad",
-      component: Pad,
+      component: Home,
     },
     {
       path: "/PC",
