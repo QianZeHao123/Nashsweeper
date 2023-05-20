2ef1b23396ecec3b750d5b205691695f9d54ac25
Qian Zehao <qianzehao123@outlook.com>
2023-05-20 13:22:39 +0800

    add CORS for ipad end

--- nashsweeper-front-ipad/vite.config.js
+++ nashsweeper-front-ipad/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+// deal with CORS
+import BackendServerInfo from './BackendServerInfo.json'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    // port: 1234,
     proxy: {
       // UploadPage CORS
       "/UploadPage": {
-        target: "http://127.0.0.1:5000",
+        // target: "http://127.0.0.1:5000",
+        target: "BackendServerInfo.backendIP",
         changeOrigin: true,
         pathRewrite: {
           "^/UploadPage": "/UploadPage",
@@ -17,7 +19,8 @@ export default defineConfig({
       },
       // GetUserData CORS
       "/GetUserData": {
-        target: "http://127.0.0.1:5000",
+        // target: "http://127.0.0.1:5000",
+        target: BackendServerInfo.backendIP,
         changeOrigin: true,
         pathRewrite: {
           "^/GetUserData": "/GetUserData",
