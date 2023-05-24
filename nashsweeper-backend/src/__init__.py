30d750002a8a09a0f4ff4c1bce7cd772c17ee203
Qian Zehao <qianzehao123@outlook.com>
2023-05-25 00:05:31 +0800

    delete some useless function

--- nashsweeper-backend/src/__init__.py
+++ nashsweeper-backend/src/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-import src.db as db
+# import src.db as db
 import src.UploadPage as UploadPage
 import src.GetUserData as GetUserData
 
@@ -8,23 +8,23 @@ import src.GetUserData as GetUserData
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'nashsweeper.sqlite'),
-    )
+    # app.config.from_mapping(
+    #     SECRET_KEY='dev',
+    #     DATABASE=os.path.join(app.instance_path, 'nashsweeper.sqlite'),
+    # )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    # if test_config is None:
+    #     # load the instance config, if it exists, when not testing
+    #     app.config.from_pyfile('config.py', silent=True)
+    # else:
+    #     # load the test config if passed in
+    #     app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    # # ensure the instance folder exists
+    # try:
+    #     os.makedirs(app.instance_path)
+    # except OSError:
+    #     pass
 
     @app.route('/hello')
     def hello():
@@ -32,7 +32,7 @@ def create_app(test_config=None):
 
     # ------------------------------------------------------------------------------
     # from . import db
-    db.init_app(app)
+    # db.init_app(app)
 
     # from . import UploadPage
     app.register_blueprint(UploadPage.bp)
