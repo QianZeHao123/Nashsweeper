b4939f6d86b3337ccbda7a489a400ddcdc8efed5
Qian Zehao <qianzehao123@outlook.com>
2023-05-24 17:28:07 +0800

    update vite profile with backend-Info import

--- nashsweeper-front/vite.config.js
+++ nashsweeper-front/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 // deal with CORS
-import BackendServerInfo from './BackendServerInfo.json'
+import BackendServerInfo from "./BackendServerInfo.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
