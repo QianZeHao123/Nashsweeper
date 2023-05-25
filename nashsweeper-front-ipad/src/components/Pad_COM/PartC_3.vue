035904d723bc05f1f943a850e665cd90ef16e891
Qian Zehao <qianzehao123@outlook.com>
2023-05-25 13:55:35 +0800

    change PartC_3 com for user log

--- nashsweeper-front-ipad/src/components/Pad_COM/PartC_3.vue
+++ nashsweeper-front-ipad/src/components/Pad_COM/PartC_3.vue
@@ -5,105 +5,105 @@
           <div>{{ time.hour }}:{{ time.minute }}:{{ time.second }}</div> -->
     <!-- <div class="text-lg text-center">Best Response Review </div> -->
     <div class="grid grid-cols-6 p-2">
-      <div class="btn btn-xs px-3" v-on:click="onClickBRPre_right()">Prev</div>
+      <div class="btn btn-xs px-3" v-on:click="onClickUserPre()">Prev</div>
       <div class="col-span-4">
-        <div class="text-base text-center">P2 NE Review</div>
+        <div class="text-base text-center">User Review</div>
       </div>
-      <div class="btn btn-xs" v-on:click="onClickBRNext_right()">Next</div>
+      <div class="btn btn-xs" v-on:click="onClickUserNext()">Next</div>
     </div>
 
 
     <div class="grid grid-rows-8">
       <div class="grid grid-cols-8">
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[0] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[1] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[2] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[3] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[4] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[5] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[6] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[7] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[0] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[1] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[2] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[3] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[4] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[5] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[6] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[7] }">&emsp;&emsp;</div>
       </div>
 
       <div class="grid grid-cols-8">
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[8] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[9] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[10] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[11] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[12] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[13] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[14] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[15] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[8] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[9] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[10] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[11] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[12] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[13] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[14] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[15] }">&emsp;&emsp;</div>
       </div>
 
       <div class="grid grid-cols-8">
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[16] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[17] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[18] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[19] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[20] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[21] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[22] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[23] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[16] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[17] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[18] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[19] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[20] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[21] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[22] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[23] }">&emsp;&emsp;</div>
       </div>
 
       <div class="grid grid-cols-8">
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[24] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[25] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[26] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[27] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[28] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[29] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[30] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[31] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[24] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[25] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[26] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[27] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[28] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[29] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[30] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[31] }">&emsp;&emsp;</div>
       </div>
 
       <div class="grid grid-cols-8">
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[32] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[33] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[34] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[35] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[36] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[37] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[38] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[39] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[32] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[33] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[34] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[35] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[36] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[37] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[38] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[39] }">&emsp;&emsp;</div>
       </div>
 
       <div class="grid grid-cols-8">
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[40] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[41] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[42] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[43] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[44] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[45] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[46] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[47] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[40] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[41] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[42] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[43] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[44] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[45] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[46] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[47] }">&emsp;&emsp;</div>
       </div>
 
       <div class="grid grid-cols-8">
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[48] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[49] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[50] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[51] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[52] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[53] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[54] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[55] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[48] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[49] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[50] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[51] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[52] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[53] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[54] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[55] }">&emsp;&emsp;</div>
       </div>
 
       <div class="grid grid-cols-8">
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[56] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[57] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[58] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[59] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[60] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[61] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[62] }">&emsp;&emsp;</div>
-        <div class="border text-xs" v-bind:class="{ 'chessbtn-green': BRstep_right[63] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[56] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[57] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[58] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[59] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[60] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[61] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[62] }">&emsp;&emsp;</div>
+        <div class="border text-xs" v-bind:class="{ 'chessbtn-brown': Userstep[63] }">&emsp;&emsp;</div>
       </div>
     </div>
 
-    <div class="text-sm text-center">TThis is No.{{ BestResponse_num_right }} Best Response</div>
+    <div class="text-sm text-center">TThis is No.{{ User_num }} Best Response</div>
   </div>
 </template>
   
@@ -116,28 +116,27 @@ export default {
   components: {},
   data() {
     const store = NsStore();
-    const { BestResponse_num_right } = storeToRefs(store);
-    const BRstep_right = new Array(64).fill(false);
-    const BRset_right = [-1, 0, 10, 20, 37, 45, 50];
+    const { Userset, User_num } = storeToRefs(store);
+    const Userstep = new Array(64).fill(false);
     return {
-      BestResponse_num_right,
-      BRstep_right,
-      BRset_right
+      User_num,
+      Userstep,
+      Userset
     };
   },
   methods: {
-    onClickBRPre_right: function () {
-      if (this.BestResponse_num_right > 0) {
-        this.BestResponse_num_right -= 1;
-        this.BRstep_right = new Array(64).fill(false);
-        this.BRstep_right[this.BRset_right[this.BestResponse_num_right]] = true
+    onClickUserPre: function () {
+      if (this.User_num > 0) {
+        this.User_num -= 1;
+        this.Userstep = new Array(64).fill(false);
+        this.Userstep[this.Userset[this.User_num]] = true
       }
     },
-    onClickBRNext_right: function () {
-      if (this.BestResponse_num_right < this.BRset_right.length - 1) {
-        this.BestResponse_num_right += 1;
-        this.BRstep_right = new Array(64).fill(false);
-        this.BRstep_right[this.BRset_right[this.BestResponse_num_right]] = true
+    onClickUserNext: function () {
+      if (this.User_num < this.Userset.length - 1) {
+        this.User_num += 1;
+        this.Userstep = new Array(64).fill(false);
+        this.Userstep[this.Userset[this.User_num]] = true
       }
     }
   },
@@ -146,8 +145,8 @@ export default {
 </script>
   
 <style scoped>
-.chessbtn-green {
-  background-color: #22c55e;
+.chessbtn-brown {
+  background-color: #cf731c;
 }
 </style>
   
