ac38dc9521c519e88d0cfb2176557bad84c2e408
Qian Zehao <qianzehao123@outlook.com>
2023-05-28 00:30:22 +0800

    test file of game data gen

--- nashsweeper-backend/src/nashsweeper_core_engine/test/test_game_calc.py
+++ nashsweeper-backend/src/nashsweeper_core_engine/test/test_game_calc.py
@@ -1,11 +1,14 @@
 import sys
+import random
 # import numpy
 sys.path.append('..')
 
 if __name__ == '__main__':
     from CoreCalcOptForGame import CoreCalc
     # import TestData
-    from TestData import test_strategy_matrix
+    # from TestData import test_strategy_matrix
+    # generate a list of 128 numbers, each of which is from 0 to 100
+    test_strategy_matrix = [random.randint(0, 100) for _ in range(128)]
     strategy_matrix = test_strategy_matrix
     regP1, regP2 = CoreCalc.regCalc(strategy_matrix)
     # calculate the regret value of regP1 and regP2
