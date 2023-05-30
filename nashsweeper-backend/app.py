f4030595654bf8b200cd71cfdec5c7fe53b869fc
Qian Zehao <qianzehao123@outlook.com>
2023-05-30 19:53:00 +0800

    db initialize

--- nashsweeper-backend/app.py
+++ nashsweeper-backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from src import GetUserData
 from src import GetGameData
+from src.utils import DBprocess
 app = Flask(__name__)
 
 # @app.route('/')
@@ -11,5 +12,8 @@ app.register_blueprint(GetUserData.bp)
 app.register_blueprint(GetGameData.bp)
 
 if __name__ == '__main__':
+
+    print("\033[31m Initialize Database \033[0m")
+    user_Record = DBprocess.createDB(
+        './instance/User.db', 'Record', '(UserName INTEGER, Time INTEGER, STEP INTEGER)')
     # app.run(debug=True)
-    app.run(debug=True)
